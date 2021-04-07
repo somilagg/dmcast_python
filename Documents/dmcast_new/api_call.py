8b7a392295e6489504a620c73d772956a7cd74d2
Somil Aggarwal <sa748@cornell.edu>
2021-04-07 19:47:10 -0400

    api call formatted

--- Documents/dmcast_new/api_call.py
+++ Documents/dmcast_new/api_call.py
@@ -2,8 +2,9 @@ import requests
 import json
 
 sid = "ny_genm nwon"
+# YYYYMMDDHH
 sdate = "2021010100"
-edate = "2021010200"
+edate = "2021010300"
 data = {
     "sid":sid,
     "sdate":sdate,
@@ -11,11 +12,16 @@ data = {
     }
 resp = requests.post('https://hrly.nrcc.cornell.edu/stnHrly', json=data)
 j = resp.json()
+j_mat = []
+print(j['hrlyFields'])
+for h in j['hrlyData']:
+    for e in range(len(h)):
+        h[e] = str(h[e])
+    j_mat.append(h)
+    print h
 
-for x in j:
-    print(j[x])
+#print(j_mat)
 
-#print(resp.json())
 
 with open('data.txt', 'w') as outfile:
     json.dump(j, outfile, indent=4, sort_keys=True)
